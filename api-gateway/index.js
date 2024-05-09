dad0bd06564b342b58cbe4ba15aa3a34d5781527
Malith Madusankha <malithsony@gmail.com>
2024-05-10 03:10:50 +0530

    add user path to api gateway

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -13,48 +13,54 @@ apiGateway.use(cors());
 const proxy = httpProxy.createProxyServer();
 
 const colors = {
-    reset: "\x1b[0m",
-    cyan: "\x1b[36m",
-    yellow: "\x1b[33m",
-    magenta: "\x1b[35m",
-    green: "\x1b[32m"
+  reset: "\x1b[0m",
+  cyan: "\x1b[36m",
+  yellow: "\x1b[33m",
+  magenta: "\x1b[35m",
+  green: "\x1b[32m",
 };
 
 // Function to log requests with color
 const consoleLog = (message, color) => {
-    console.log(`${color}${message}${colors.reset}`);
+  console.log(`${color}${message}${colors.reset}`);
 };
 
-apiGateway.use('/api/auth', (req, res) => {
-    consoleLog(`Request sent to auth server from gateway`, colors.green);
-    proxy.web(req, res, { target: process.env.AUTH_API });
+apiGateway.use("/api/auth", (req, res) => {
+  consoleLog(`Request sent to auth server from gateway`, colors.green);
+  proxy.web(req, res, { target: process.env.AUTH_API });
 });
 
-apiGateway.use('/api/*', (req, res, next) => {
-    authenticate(req, res, next);
+apiGateway.use("/api/*", (req, res, next) => {
+  authenticate(req, res, next);
 });
 
-proxy.on('error', (error, req, res) => {
-    console.error('Proxy Error:', error);
-    res.status(500).send('Proxy Error');
+proxy.on("error", (error, req, res) => {
+  console.error("Proxy Error:", error);
+  res.status(500).send("Proxy Error");
 });
 
 apiGateway.listen(process.env.API_GATEWAY_PORT, () => {
-    console.log(`API Gateway running on http://localhost:${process.env.API_GATEWAY_PORT}`);
+  console.log(
+    `API Gateway running on http://localhost:${process.env.API_GATEWAY_PORT}`
+  );
 });
 
-apiGateway.use('/api/payment', (req, res) => {
-    consoleLog(`Request sent to payment server from gateway`, colors.cyan);
-    proxy.web(req, res, { target: process.env.PAYMENT_API });
+apiGateway.use("/api/user", (req, res) => {
+  consoleLog(`Request sent to auth server from gateway`, colors.green);
+  proxy.web(req, res, { target: process.env.AUTH_API });
 });
 
-apiGateway.use('/api/course', (req, res) => {
-    consoleLog(`Request sent to course server from gateway`, colors.yellow);
-    proxy.web(req, res, { target: process.env.COURSE_API });
+apiGateway.use("/api/payment", (req, res) => {
+  consoleLog(`Request sent to payment server from gateway`, colors.cyan);
+  proxy.web(req, res, { target: process.env.PAYMENT_API });
 });
 
-apiGateway.use('/api/learner', (req, res) => {
-    consoleLog(`Request sent to learner server from gateway`, colors.magenta);
-    proxy.web(req, res, { target: process.env.LEARNER_API });
-}); 
+apiGateway.use("/api/course", (req, res) => {
+  consoleLog(`Request sent to course server from gateway`, colors.yellow);
+  proxy.web(req, res, { target: process.env.COURSE_API });
+});
 
+apiGateway.use("/api/learner", (req, res) => {
+  consoleLog(`Request sent to learner server from gateway`, colors.magenta);
+  proxy.web(req, res, { target: process.env.LEARNER_API });
+});
