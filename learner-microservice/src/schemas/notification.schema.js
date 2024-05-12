eb0f798834cb83d375cb9c33d65532a45f108e8f
IT21302862 <sanayasamadhi@gmail.com>
2024-05-12 17:41:56 +0530

    notification getter for user id completed.

--- learner-microservice/src/schemas/notification.schema.js
+++ learner-microservice/src/schemas/notification.schema.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const emailNotificationSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
+    ref :"User",
     required: true
   },
   senderEmail: {
