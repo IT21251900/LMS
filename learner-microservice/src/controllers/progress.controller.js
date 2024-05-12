1f7665f2f68de5989a9e5eef044e2164e99e3882
IT21302862 <sanayasamadhi@gmail.com>
2024-05-12 12:59:34 +0530

    progress get function completed

--- learner-microservice/src/controllers/progress.controller.js
+++ learner-microservice/src/controllers/progress.controller.js
@@ -1,4 +1,5 @@
-import CourseProgress from '../schemas/lessons.schema.js';
+import CourseProgress from "../schemas/lessons.schema.js";
+import User from "../schemas/user.schema.js";
 
 async function updateLessonProgress(req, res) {
   try {
@@ -7,25 +8,63 @@ async function updateLessonProgress(req, res) {
     const courseProgress = await CourseProgress.findOne({ userId, courseId });
 
     if (!courseProgress) {
-      return res.status(404).json({ message: 'Course progress not found' });
+      return res.status(404).json({ message: "Course progress not found" });
     }
     const lessonIndex = courseProgress.lessons.findIndex(
-      lesson => lesson.lessonId.toString() === lessonId
+      (lesson) => lesson.lessonId.toString() === lessonId
     );
 
     if (lessonIndex === -1) {
-      return res.status(404).json({ message: 'Lesson not found in course progress' });
+      return res
+        .status(404)
+        .json({ message: "Lesson not found in course progress" });
     }
 
     courseProgress.lessons[lessonIndex].progress = progress;
-    
+
     await courseProgress.save();
 
-    res.status(200).json({ message: 'Lesson progress updated successfully' });
+    res.status(200).json({ message: "Lesson progress updated successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
   }
 }
 
-export { updateLessonProgress };
+async function getLessonProgressByLessonId(req, res) {
+  try {
+    const { userId, courseId, lessonId } = req.params; 
+
+    console.log("userId:", userId);
+    console.log("courseId:", courseId);
+    console.log("lessonId:", lessonId);
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const courseProgress = await CourseProgress.findOne({ userId, courseId });
+    if (!courseProgress) {
+      return res.status(404).json({ message: "Course progress not found" });
+    }
+
+    const lessonProgress = courseProgress.lessons.find(
+      (lesson) => lesson.lessonId.toString() === lessonId
+    );
+
+    if (!lessonProgress) {
+      return res.status(404).json({ message: "Lesson progress not found" });
+    }
+
+    res.status(200).json({ lessonProgress });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+}
+
+
+
+
+export { updateLessonProgress, getLessonProgressByLessonId };
