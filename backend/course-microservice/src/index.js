f8f3366e6e54465688459a19f695ec7077647877
Sanjana Dinith <100860580+IT21251900@users.noreply.github.com>
2024-05-13 21:39:27 +0530

    dockerized frontend

--- backend/course-microservice/src/index.js
+++ backend/course-microservice/src/index.js
@@ -37,7 +37,7 @@ courseService.use(cookieParser());
 courseService.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:5100",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:5173",
     ],
